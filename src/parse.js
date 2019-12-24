b7c5ed344b9072725f6c63e7ac901c992e8edcc8
fluxet <fluxet@mail.ru>
2019-12-24 16:59:29 +0200

    отделение побочных эффектов от функции парсинга

--- src/parse.js
+++ src/parse.js
@@ -1,16 +1,10 @@
-import fs from 'fs';
 import yaml from 'js-yaml';
-import path from 'path';
 import ini from 'ini';
 
-const extToObj = {
-  '.yml': yaml.safeLoad,
-  '.json': JSON.parse,
-  '.ini': ini.parse,
+const formatToObj = {
+  yaml: yaml.safeLoad,
+  json: JSON.parse,
+  ini: ini.parse,
 };
 
-export default (filePath) => {
-  const ext = path.extname(filePath);
-  const content = fs.readFileSync(filePath, 'utf-8');
-  return extToObj[ext](content);
-};
+export default (content, format) => formatToObj[format](content);
